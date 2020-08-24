2ff80fbdd7aa69439b3226fe85d3f5b083c5d35d
ciandre23 <ciandre23@gmail.com>
2020-08-24 18:56:30 +0200

    pocetna faze

---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ArticleList extends Component {
+    
+    render() { 
+        return ( 
+            <div className='aricle-list'>
+                <ul>
+                    <li>Article 1</li>
+                    <li>Article 2</li>
+                    <li>Article 3</li>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default ArticleList;
+
