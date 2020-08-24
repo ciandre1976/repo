6a33d2ced08be39e53f2f3beaff8c0cef9b0b978
ciandre23 <ciandre23@gmail.com>
2020-08-24 20:39:13 +0200

    add theme and using data

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { Theme } from './contexts/Theme';
 
 class ArticleList extends Component {
-    
+    static contextType=Theme;
+
     render() { 
+        const {isLightTheme,light,dark}=this.context;
+        const theme=isLightTheme?light:dark;
         return ( 
-            <div className='aricle-list'>
+            <div className='aricle-list' style={{background:theme.ui,color:theme.syntax}}>
                 <ul>
-                    <li>Article 1</li>
-                    <li>Article 2</li>
-                    <li>Article 3</li>
+                    <li style={{background:theme.bg}}>Article 1</li>
+                    <li style={{background:theme.bg}}>Article 2</li>
+                    <li style={{background:theme.bg}}>Article 3</li>
                 </ul>
             </div>
          );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import { Theme } from './contexts/Theme';
 
 class Navbar extends Component {
-    
+    static contextType=Theme;
     render() { 
+        const {isLightTheme,light,dark}=this.context;   
+        const theme=isLightTheme?light:dark;
+
         return ( 
-            <nav>
+            <nav style={{background:theme.ui,color:theme.syntax}}>
                 <h1>Trial App</h1>
                 <ul>
                     <li>Home</li>
