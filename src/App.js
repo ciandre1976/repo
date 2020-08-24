164fe1fb765c9c2b36996cc7ba683db3eef73925
ciandre23 <ciandre23@gmail.com>
2020-08-24 21:35:34 +0200

    adding auth provider

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Navbar from './components/Navbar';
 import ArticleList from './components/ArticleList';
 import ThemeProvider from './components/contexts/Theme';
 import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './components/contexts/AuthContext';
 
 
 function App() {
   return (
     <div className='App'>
     <ThemeProvider>
-      <Navbar />
-      <ArticleList />
-      <ThemeToggle />
+     <AuthContextProvider>
+        <Navbar />
+        <ArticleList />
+        <ThemeToggle />
+      </AuthContextProvider>
     </ThemeProvider>
     </div>
-
   );
 }
 
