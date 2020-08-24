644ab8142b8a480ceb1d1c2b42a0bf9ce2f98120
ciandre23 <ciandre23@gmail.com>
2020-08-24 19:58:30 +0200

    add context provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import ArticleList from './components/ArticleList';
+import ThemeProvider from './components/contexts/Theme';
 
 
 function App() {
   return (
     <div className='App'>
+    <ThemeProvider>
       <Navbar />
       <ArticleList />
+    </ThemeProvider>
     </div>
+
   );
 }
 
